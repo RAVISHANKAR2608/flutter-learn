715e7b5f29347b35d61685ab57c5687f5242ab29
RAVISHANKAR2608 <ravisaravanan2608@gmail.com>
2024-04-21 18:16:23 +0530

    Bottom Navigation Added

---
+++ lib/screens/calls.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Calls extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calls'),
+      ),
+      body: Center(
+        child: Text('Calls Page'),
+      ),
+    );
+  }
+}
